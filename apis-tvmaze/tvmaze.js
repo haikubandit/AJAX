5f49c39eff603521d1452bbdf01acd3ea15038a3
Daniel Knapp <knapparatus@gmail.com>
2021-01-30 23:44:40 -0600

    completed tv maze images

--- apis-tvmaze/tvmaze.js
+++ apis-tvmaze/tvmaze.js
@@ -19,26 +19,22 @@
 async function searchShows(query) {
 	// TODO: Make an ajax request to the searchShows api.  Remove
 	// hard coded data.
-	const showsList = [];
-	const shows = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
-	console.log(shows);
-	// const { data } = shows;
-	for (const show of shows.data) {
-		// const { id, name, summary, image: { original: image } } = show.show;
-		const { id, name, summary } = show.show;
-		let image = '';
-		console.log('original', show.show.image.original);
-		if (!show.show.image) {
-			image = 'https://tinyurl.com/tv-missing';
-		} else {
-			image = show.show.image.original;
-		}
+	// const showsList = [];
+	const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
+	// console.log(shows);
 
-		// console.log(show.show.image);
-		showsList.push({ id, name, summary, image });
-	}
-	console.log(showsList);
-	return showsList;
+	let shows = response.data.map(result => {
+		let show = result.show;
+		const { id, name, summary } = result.show;
+		return {
+			id,
+			name,
+			summary,
+			image: show.image ? show.image.original : 'https://tinyurl.com/tv-missing'
+		};
+	});
+
+	return shows;
 }
 
 /** Populate shows list:
@@ -53,9 +49,9 @@ function populateShows(shows) {
 		let $item = $(
 			`<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
+         <img class="card-img-top" src="${show.image}" alt="${show.name}-image">
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
-             <img class="card-img-top" src="${show.image}" alt="${show.name}-image">
              <p class="card-text">${show.summary}</p>
            </div>
          </div>

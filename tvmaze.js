767033e27bc1e0b727e1b5896e47c3e759020965
Daniel Knapp <knapparatus@gmail.com>
2021-01-30 23:22:21 -0600

    added dynamic movie data, dev on resolving null images

--- tvmaze.js
+++ tvmaze.js
@@ -2,7 +2,6 @@
  *     { id, name, summary, episodesUrl }
  */
 
-
 /** Search Shows
  *    - given a search term, search for tv shows that
  *      match that query.  The function is async show it
@@ -18,73 +17,81 @@
       }
  */
 async function searchShows(query) {
-  // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
+	// TODO: Make an ajax request to the searchShows api.  Remove
+	// hard coded data.
+	const showsList = [];
+	const shows = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
+	console.log(shows);
+	// const { data } = shows;
+	for (const show of shows.data) {
+		// const { id, name, summary, image: { original: image } } = show.show;
+		const { id, name, summary } = show.show;
+		let image = '';
+		console.log('original', show.show.image.original);
+		if (!show.show.image) {
+			image = 'https://tinyurl.com/tv-missing';
+		} else {
+			image = show.show.image.original;
+		}
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+		// console.log(show.show.image);
+		showsList.push({ id, name, summary, image });
+	}
+	console.log(showsList);
+	return showsList;
 }
-
-
 
 /** Populate shows list:
  *     - given list of shows, add shows to DOM
  */
 
 function populateShows(shows) {
-  const $showsList = $("#shows-list");
-  $showsList.empty();
+	const $showsList = $('#shows-list');
+	$showsList.empty();
 
-  for (let show of shows) {
-    let $item = $(
-      `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
+	for (let show of shows) {
+		let $item = $(
+			`<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
+             <img class="card-img-top" src="${show.image}" alt="${show.name}-image">
              <p class="card-text">${show.summary}</p>
            </div>
          </div>
        </div>
-      `);
+      `
+		);
 
-    $showsList.append($item);
-  }
+		$showsList.append($item);
+	}
 }
-
 
 /** Handle search form submission:
  *    - hide episodes area
  *    - get list of matching shows and show in shows list
  */
 
-$("#search-form").on("submit", async function handleSearch (evt) {
-  evt.preventDefault();
+$('#search-form').on('submit', async function handleSearch(evt) {
+	evt.preventDefault();
 
-  let query = $("#search-query").val();
-  if (!query) return;
+	let query = $('#search-query').val();
+	if (!query) return;
 
-  $("#episodes-area").hide();
+	$('#episodes-area').hide();
 
-  let shows = await searchShows(query);
+	let shows = await searchShows(query);
 
-  populateShows(shows);
+	populateShows(shows);
 });
-
 
 /** Given a show ID, return list of episodes:
  *      { id, name, season, number }
  */
 
 async function getEpisodes(id) {
-  // TODO: get episodes from tvmaze
-  //       you can get this by making GET request to
-  //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
-
-  // TODO: return array-of-episode-info, as described in docstring above
+	// TODO: get episodes from tvmaze
+	//       you can get this by making GET request to
+	//       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
+	// TODO: return array-of-episode-info, as described in docstring above
 }
